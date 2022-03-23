0c2286490330989a21b296c2eea493e921e3c2c7
Adrien Leib <adrienleib@MacBook-Pro-de-Adrien.local>
2022-03-23 14:52:27 +0100

    work progress

--- routes/professionalRoute.py
+++ routes/professionalRoute.py
@@ -6,8 +6,6 @@ professional_bp = Blueprint('professional_bp', __name__)
 professional_bp.route('/create', methods=['POST'])(create)
 professional_bp.route('/', methods=['GET'])(getAll)
 professional_bp.route('/find', methods=['GET'])(getProfessionalBySpeciality)
-professional_bp.route('/add/<professional_id>', methods=['GET'])(addPatientToProfessional)
-
-
+professional_bp.route('/add/<professional_id>', methods=['POST'])(addPatientToProfessional)
 professional_bp.route('/<int:professional_id>/edit', methods=['POST'])(update)
 professional_bp.route('/<int:professional_id>', methods=['DELETE'])(delete)
